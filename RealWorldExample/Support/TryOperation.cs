da0e0b54086591a3cbe769352db4d9b2af81ecf1
Christian Aranda <kristian.aranda@gmail.com>
2023-05-03 16:30:08 +0200

    refactor

--- RealWorldExample/Support/TryOperation.cs
+++ RealWorldExample/Support/TryOperation.cs
@@ -6,7 +6,7 @@ public delegate Exception? ExceptionPredicate(Exception ex);
 
 public class TryOperation
 {
-    public ErrorResult? ErrorResult { get; set; }
+    public ErrorResult? ErrorResult { get; set; } = new("Error", "unknown error");
 
     public ICollection<ExceptionPredicate?> ExceptionPredicateCollection { get; internal set; } = new List<ExceptionPredicate?>();
 
@@ -26,40 +26,4 @@ public sealed class TryOperation<T> : TryOperation
     public T Value { get; set; }
 
     public TryOperation(T value) => this.Value = value;
-}
-
-public static class TryOperationExtensions
-{
-    public static TryOperation<T> For<T>(this TryOperation tryOperation, T value)
-    {
-        TryOperation<T> newlyTryOperation = new(value)
-        {
-            ExceptionPredicateCollection = tryOperation.NotNull().ExceptionPredicateCollection
-        };
-        return newlyTryOperation;
-    }
-
-    public static TryOperation<T> WithError<T>(this TryOperation<T> builder, ErrorResult errorResult)
-    {
-        builder.NotNull().ErrorResult = errorResult;
-        return builder;
-    }
-
-    public static Result<TR> Try<T, TR>(this TryOperation<T> builder, Func<T, TR> action)
-    {
-        try
-        {
-            return action.NotNull()(builder.NotNull().Value);
-        }
-        catch (Exception e)
-        {
-            ExceptionPredicate? exception = builder.NotNull().ExceptionPredicateCollection.FirstOrDefault(x => x?.Invoke(e) is not null);
-            if (exception is null)
-            {
-                throw;
-            }
-
-            return builder.ErrorResult ?? new ErrorResult("Error", "unknown error");
-        }
-    }
 }
---
+++ RealWorldExample/Support/TryOperationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using RealWorldExample.Results;
+
+namespace RealWorldExample.Support;
+
+public static class TryOperationExtensions
+{
+    public static TryOperation<T> For<T>(this TryOperation tryOperation, T value)
+    {
+        TryOperation<T> newlyTryOperation = new(value)
+        {
+            ExceptionPredicateCollection = tryOperation.NotNull().ExceptionPredicateCollection
+        };
+        return newlyTryOperation;
+    }
+
+    public static TryOperation<T> WithError<T>(this TryOperation<T> builder, ErrorResult errorResult)
+    {
+        builder.NotNull().ErrorResult = errorResult;
+        return builder;
+    }
+
+    public static Result<TR> Try<T, TR>(this TryOperation<T> builder, Func<T, TR> action)
+    {
+        try
+        {
+            return action.NotNull()(builder.NotNull().Value);
+        }
+        catch (Exception e)
+        {
+            ExceptionPredicate? exception = builder.NotNull().ExceptionPredicateCollection.FirstOrDefault(x => x?.Invoke(e) is not null);
+            if (exception is null)
+            {
+                throw;
+            }
+
+            return builder.ErrorResult;
+        }
+    }
+}
